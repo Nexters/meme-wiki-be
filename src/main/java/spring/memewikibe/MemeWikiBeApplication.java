bd051b97c07b05c67aac1d521f66358a1e0b4542
RyooChan <fbcks97@naver.com>
2025-07-13 00:19:07 +0900

    🎉 Init: Initial commit

---
+++ src/main/java/spring/memewikibe/MemeWikiBeApplication.java
@@ -0,0 +1,13 @@
+package spring.memewikibe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeWikiBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeWikiBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/memewikibe/MemeWikiBeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.memewikibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeWikiBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
