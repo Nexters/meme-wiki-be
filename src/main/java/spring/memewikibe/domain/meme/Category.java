bfd59de08b5df23dd3a60cd19e1ef13d256c6e06
NaMinhyeok <nmh9097@gmail.com>
2025-07-26 16:29:18 +0900

    feat: 테이블 설명을 상세히 하고, 카테고리를 테이블을 분리한다

--- src/main/java/spring/memewikibe/domain/meme/Category.java
+++ src/main/java/spring/memewikibe/domain/meme/Category.java
@@ -1,22 +1,16 @@
 package spring.memewikibe.domain.meme;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import spring.memewikibe.domain.BaseEntity;
 
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Meme extends BaseEntity {
-    private String title;
-
-    private String origin;
-
-    private String usageContext;
-
+public class Category extends BaseEntity {
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false)
     private String imgUrl;
-
-    private String hashtags;
 }
