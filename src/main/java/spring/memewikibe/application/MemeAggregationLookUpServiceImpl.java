6cd6a224cf45f2d0b566eff50e503810e0da4ab7
NaMinhyeok <nmh9097@gmail.com>
2025-07-29 18:15:57 +0900

    feat: 종합 스코어링된 목록을 조회하는 기능을 구현한다

--- src/main/java/spring/memewikibe/application/MemeAggregationLookUpServiceImpl.java
+++ src/main/java/spring/memewikibe/application/MemeAggregationLookUpServiceImpl.java
@@ -3,7 +3,9 @@ package spring.memewikibe.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import spring.memewikibe.api.controller.meme.response.MemeSimpleResponse;
+import spring.memewikibe.domain.meme.MemeAggregationResult;
 import spring.memewikibe.infrastructure.MemeCustomLogRepository;
+import spring.memewikibe.infrastructure.MemeRepository;
 import spring.memewikibe.infrastructure.MemeShareLogRepository;
 import spring.memewikibe.infrastructure.MemeViewLogRepository;
 
@@ -15,15 +17,18 @@ public class MemeAggregationLookUpServiceImpl implements MemeAggregationLookUpSe
 
     private final static Duration AGGREGATION_DURATION_PERIOD = Duration.ofDays(1);
     private final static int AGGREGATION_ITEM_COUNT = 10;
+    private final static int MOST_POPULAR_MEMES_COUNT = 6;
 
     private final MemeCustomLogRepository customLogRepository;
     private final MemeViewLogRepository viewLogRepository;
     private final MemeShareLogRepository shareLogRepository;
+    private final MemeRepository memeRepository;
 
-    public MemeAggregationLookUpServiceImpl(MemeCustomLogRepository customLogRepository, MemeViewLogRepository viewLogRepository, MemeShareLogRepository shareLogRepository) {
+    public MemeAggregationLookUpServiceImpl(MemeCustomLogRepository customLogRepository, MemeViewLogRepository viewLogRepository, MemeShareLogRepository shareLogRepository, MemeRepository memeRepository) {
         this.customLogRepository = customLogRepository;
         this.viewLogRepository = viewLogRepository;
         this.shareLogRepository = shareLogRepository;
+        this.memeRepository = memeRepository;
     }
 
 
@@ -39,8 +44,12 @@ public class MemeAggregationLookUpServiceImpl implements MemeAggregationLookUpSe
         return customLogRepository.findTopMemesByCustomCountWithin(AGGREGATION_DURATION_PERIOD, AGGREGATION_ITEM_COUNT);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<MemeSimpleResponse> getMostPopularMemes() {
-        return List.of();
+        List<MemeAggregationResult> aggregationResult = memeRepository.findTopRatedMemesBy(AGGREGATION_DURATION_PERIOD, MOST_POPULAR_MEMES_COUNT);
+        return aggregationResult.stream()
+            .map(it -> new MemeSimpleResponse(it.id(), it.title(), it.imgUrl()))
+            .toList();
     }
 }
--- src/main/java/spring/memewikibe/infrastructure/MemeRepository.java
+++ src/main/java/spring/memewikibe/infrastructure/MemeRepository.java
@@ -3,5 +3,5 @@ package spring.memewikibe.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.memewikibe.domain.meme.Meme;
 
-public interface MemeRepository extends JpaRepository<Meme, Long> {
+public interface MemeRepository extends JpaRepository<Meme, Long>, MemeAggregationRepository {
 }
