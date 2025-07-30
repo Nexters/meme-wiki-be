e612b6098018602a606d3336f1b57a98244de60e
NaMinhyeok <nmh9097@gmail.com>
2025-07-30 21:34:34 +0900

    feat: 밈을 상세조회하면 조회 로그를 비동기로 증적한다

--- src/main/java/spring/memewikibe/application/MemeAggregationServiceImpl.java
+++ src/main/java/spring/memewikibe/application/MemeAggregationServiceImpl.java
@@ -1,10 +1,15 @@
 package spring.memewikibe.application;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
 import spring.memewikibe.domain.meme.Meme;
 import spring.memewikibe.domain.meme.MemeCustomLog;
 import spring.memewikibe.domain.meme.MemeShareLog;
 import spring.memewikibe.domain.meme.MemeViewLog;
+import spring.memewikibe.domain.meme.event.MemeViewedEvent;
 import spring.memewikibe.infrastructure.MemeCustomLogRepository;
 import spring.memewikibe.infrastructure.MemeRepository;
 import spring.memewikibe.infrastructure.MemeShareLogRepository;
@@ -44,6 +49,13 @@ public class MemeAggregationServiceImpl implements MemeAggregationService {
     public void increaseShareMemeCount(Long memeId) {
         Meme meme = getMemeBy(memeId);
         memeShareLogRepository.save(MemeShareLog.of(meme));
+    }
+
+    @Async
+    @TransactionalEventListener
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handleMemeViewed(MemeViewedEvent event) {
+        increaseMemeViewCount(event.memeId());
     }
 
     private Meme getMemeBy(Long memeId) {
