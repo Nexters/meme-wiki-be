2baf19f0636210ed39d2d32f5847aebe0b53e64a
RyooChan <fbcks97@naver.com>
2025-07-31 17:56:37 +0900

    feat: swagger https

--- src/main/java/spring/memewikibe/config/SwaggerConfig.java
+++ src/main/java/spring/memewikibe/config/SwaggerConfig.java
@@ -2,19 +2,45 @@ package spring.memewikibe.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${spring.profiles.active:local}")
+    private String activeProfile;
+
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
+        OpenAPI openAPI = new OpenAPI()
             .info(new Info()
                 .title("MemeWikiBE API")
                 .version("v0.0.1")
                 .description("MemeWikiBE API 명세서"));
+
+        if ("prod".equals(activeProfile)) {
+            openAPI.servers(List.of(
+                new Server()
+                    .url("https://meme-wiki.net")
+                    .description("Production Server (HTTPS)")
+            ));
+        } else {
+            openAPI.servers(List.of(
+                new Server()
+                    .url("http://localhost:8080")
+                    .description("Local Development Server (HTTP)"),
+                new Server()
+                    .url("https://localhost:8080")
+                    .description("Local Development Server (HTTPS)")
+            ));
+        }
+
+        return openAPI;
     }
 
 }
