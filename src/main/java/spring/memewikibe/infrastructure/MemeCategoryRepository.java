306bce7d37c71c145499ce66abe5c0f95b8f9438
RyooChan <fbcks97@naver.com>
2025-07-31 17:32:20 +0900

    refactor: 리뷰 반영
    - DB call 2 -> 1회
    - ASC -> DESC
    - get list limit

--- src/main/java/spring/memewikibe/infrastructure/MemeCategoryRepository.java
+++ src/main/java/spring/memewikibe/infrastructure/MemeCategoryRepository.java
@@ -2,14 +2,20 @@ package spring.memewikibe.infrastructure;
 
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spring.memewikibe.domain.meme.Category;
-import spring.memewikibe.domain.meme.Meme;
 import spring.memewikibe.domain.meme.MemeCategory;
 
 import java.util.List;
 
 public interface MemeCategoryRepository extends JpaRepository<MemeCategory, Long> {
-    List<MemeCategory> findByCategoryAndMemeGreaterThanOrderByMemeAsc(Category category, Meme meme, Limit limit);
+    @Query("SELECT mc FROM MemeCategory mc WHERE mc.category = :category AND mc.meme.id < :lastMemeId ORDER BY mc.meme.id DESC")
+    List<MemeCategory> findByCategoryAndMemeGreaterThanOrderByMemeDesc(
+        @Param("category") Category category, 
+        @Param("lastMemeId") Long lastMemeId, 
+        Limit limit
+    );
 
-    List<MemeCategory> findByCategory(Category category, Limit limit);
+    List<MemeCategory> findByCategory(@Param("category") Category category, Limit limit);
 }
