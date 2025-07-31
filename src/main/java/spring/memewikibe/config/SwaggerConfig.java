903b67c3181ee7529da415ce7324c4d4969f1644
RyooChan <fbcks97@naver.com>
2025-07-31 22:25:56 +0900

    feat: prod 에 mysql 생성 및 swagger 접근 가능하도록 설정

--- src/main/java/spring/memewikibe/config/SwaggerConfig.java
+++ src/main/java/spring/memewikibe/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package spring.memewikibe.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,7 +17,11 @@ public class SwaggerConfig {
             .info(new Info()
                 .title("MemeWikiBE API")
                 .version("v0.0.1")
-                .description("MemeWikiBE API 명세서"));
+                .description("MemeWikiBE API 명세서"))
+            .servers(List.of(
+                new Server()
+                    .url("https://api.meme-wiki.net")
+                    .description("Production Server (HTTPS)")
+            ));
     }
-
 }
