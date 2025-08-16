e9902d5d0b0d1c196ab5712411f44e891d628791
RyooChan <fbcks97@naver.com>
2025-08-17 05:16:55 +0900

    feat: 밈 집어넣는 admin 페이지 추가

--- src/main/java/spring/memewikibe/infrastructure/MemeRepository.java
+++ src/main/java/spring/memewikibe/infrastructure/MemeRepository.java
@@ -3,5 +3,8 @@ package spring.memewikibe.infrastructure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.memewikibe.domain.meme.Meme;
 
+import java.util.List;
+
 public interface MemeRepository extends JpaRepository<Meme, Long>, MemeAggregationRepository {
+    List<Meme> findAllByOrderByIdDesc();
 }
