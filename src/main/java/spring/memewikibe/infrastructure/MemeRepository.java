9023e7254014dd1d42f982f844d97d6a8e3fe8f1
NaMinhyeok <nmh9097@gmail.com>
2025-08-18 20:24:33 +0900

    feat: 어드민에서 카테고리 조회 추가

--- src/main/java/spring/memewikibe/infrastructure/MemeRepository.java
+++ src/main/java/spring/memewikibe/infrastructure/MemeRepository.java
@@ -1,10 +1,17 @@
 package spring.memewikibe.infrastructure;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import spring.memewikibe.domain.meme.Meme;
 
 import java.util.List;
 
 public interface MemeRepository extends JpaRepository<Meme, Long>, MemeAggregationRepository {
     List<Meme> findAllByOrderByIdDesc();
+    
+    @Query("SELECT DISTINCT m, c.name FROM Meme m " +
+           "LEFT JOIN MemeCategory mc ON m.id = mc.meme.id " +
+           "LEFT JOIN Category c ON mc.category.id = c.id " +
+           "ORDER BY m.id DESC")
+    List<Object[]> findAllWithCategoryNamesOrderByIdDesc();
 }
