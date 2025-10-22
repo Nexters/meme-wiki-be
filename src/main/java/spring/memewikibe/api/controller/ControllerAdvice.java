73d9c39af6fb9b05f9f6fd2531ee6a8e3d14f8f9
NaMinhyeok <nmh9097@gmail.com>
2025-10-22 13:14:22 +0900

    사용하지 않는 reranker 관련 로직을 삭제한다
    - reranker 시범 API 삭제 더이상 사용하지 않음

--- src/main/java/spring/memewikibe/api/controller/ControllerAdvice.java
+++ src/main/java/spring/memewikibe/api/controller/ControllerAdvice.java
@@ -7,12 +7,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
-import spring.memewikibe.external.exception.ClovaApiException;
 import spring.memewikibe.support.error.ErrorType;
 import spring.memewikibe.support.error.MemeWikiApplicationException;
 import spring.memewikibe.support.response.ApiResponse;
-
-import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerAdvice {
@@ -38,34 +35,6 @@ public class ControllerAdvice {
         log.warn("Static resource not found: {}", e.getMessage());
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
-
-    @ExceptionHandler(ClovaApiException.class)
-    public ResponseEntity<ApiResponse<?>> handleClovaApiException(ClovaApiException e) {
-        // 로그에는 상세한 Clova 에러 정보 기록
-        log.error("Clova API Error - Code: {}, Category: {}, Retryable: {}, Message: {}",
-            e.getClovaCode(), e.getCategory(), e.isRetryable(), e.getMessage(), e);
-
-        ErrorType errorType = switch (e.getCategory()) {
-            case AUTHENTICATION -> ErrorType.EXTERNAL_SERVICE_UNAUTHORIZED;
-            case RATE_LIMIT -> ErrorType.EXTERNAL_SERVICE_TOO_MANY_REQUESTS;
-            case CLIENT_ERROR -> ErrorType.EXTERNAL_SERVICE_BAD_REQUEST;
-            default -> ErrorType.EXTERNAL_SERVICE_ERROR;
-        };
-
-        var errorData = Map.of(
-            "clovaCode", e.getClovaCode(),
-            "clovaOriginalMessage", e.getClovaMessage(),
-            "category", e.getCategory().name(),
-            "retryable", e.isRetryable()
-        );
-
-        return new ResponseEntity<>(
-            ApiResponse.error(errorType, errorData),
-            errorType.getStatus()
-        );
-    }
-
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
